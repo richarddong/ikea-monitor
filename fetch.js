ccdee90098b32eaa926b84b8518cb34357193e1e
Shaotian Dong <me@dong.st>
2020-05-25 11:19:35 -0700

    Wait 1000 ms before loading new stores list

--- fetch.js
+++ fetch.js
@@ -190,11 +190,11 @@ function update(store, status) {
 }
 
 fs.watch('./data/us-stores-list.json', (eventType, filename) => {
-  if (eventType == 'change') load_stores_list('us');
+  if (eventType == 'change') setTimeout(load_stores_list, 1000, 'us');
 });
 
 fs.watch('./data/ca-stores-list.json', (eventType, filename) => {
-  if (eventType == 'change') load_stores_list('ca');
+  if (eventType == 'change') setTimeout(load_stores_list, 1000, 'ca');
 });
 
 // ========================================
