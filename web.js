42dd26f96a6c52a1e86eab4c5fc3d33b5a3d2b1e
Shaotian Dong <me@dong.st>
2020-05-25 13:57:05 -0700

    Switch from node-static to serve-static

--- web.js
+++ web.js
@@ -1,10 +1,26 @@
-var static = require('node-static');
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
 
-var file = new static.Server('./public', {cache: 30});
+// Serve up public/ftp folder
+var serve = serveStatic('public', { 'index': ['index.html', 'index.htm'] })
 
-require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response);
-  }).resume();
-}).listen(80);
+// Create server
+var server = http.createServer(function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+})
+
+// Listen
+server.listen(80)
+
+
+// var static = require('node-static');
+
+// var file = new static.Server('./public', {cache: 30});
+
+// require('http').createServer(function (request, response) {
+//   request.addListener('end', function () {
+//     file.serve(request, response);
+//   }).resume();
+// }).listen(80);
 
